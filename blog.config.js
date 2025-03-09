db6c1c6c7f4a161413e6a8bb0dffdee9b08883ed
LiamNightLife <133378792+LiamNightLife@users.noreply.github.com>
2025-03-09 22:06:55 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Liam的泰语学习', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '提高来泰司机的体验度', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://liamthai.club', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Soi6, Pattaya, 六巷, 曼谷泰浴, 芭提雅, 日式按摩, 口语课, 泰国上门, 曼谷上门, 芭提雅上门, 延时, 色情按摩, Agogo, 啤酒吧, 泰国夜生活', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
