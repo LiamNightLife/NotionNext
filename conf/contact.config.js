3b696552c62eca9fd91674dbc7de8e314497ad5a
LiamNightLife <133378792+LiamNightLife@users.noreply.github.com>
2025-10-10 17:38:00 +0700

    Update contact.config.js

--- conf/contact.config.js
+++ conf/contact.config.js
@@ -20,7 +20,7 @@ module.exports = {
   CONTACT_TIKTOK: process.env.NEXT_PUBLIC_CONTACT_TIKTOK || 'https://v.douyin.com/UAxKQCy/d', // Tiktok主页
   CONTACT_XIAOHONGSHU: process.env.NEXT_PUBLIC_CONTACT_XIAOHONGSHU || 'https://www.xiaohongshu.com/user/profile/609833d30000000001007e0b?xhsshare=CopyLink&appuid=609833d30000000001007e0b&apptime=1714647466', // 小红书主页
   CONTACT_WEIXIN: process.env.NEXT_PUBLIC_CONTACT_WEIXIN || 'https://liamthai.club/WeChat', // 微信主页
-  CONTACT_LINE: process.env.NEXT_PUBLIC_CONTACT_LINE || 'https://line.me/ti/p/85Hl6n8t71' ,  // line主页
+  CONTACT_LINE: process.env.NEXT_PUBLIC_CONTACT_LINE || 'https://line.me/ti/p/kbsNVnkSWC' ,  // line主页
   CONTACT_XIAOHONGSHU: process.env.NEXT_PUBLIC_CONTACT_XIAOHONGSHU || '', // 小红书主页
   CONTACT_ZHISHIXINGQIU: process.env.NEXT_PUBLIC_CONTACT_ZHISHIXINGQIU || '', // 知识星球
   CONTACT_WEHCHAT_PUBLIC: process.env.NEXT_PUBLIC_CONTACT_WEHCHAT_PUBLIC || '' // 微信公众号 格式：https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=【xxxxxx】==#wechat_redirect
